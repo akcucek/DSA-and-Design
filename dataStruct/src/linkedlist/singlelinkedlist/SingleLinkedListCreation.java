6b9da668332e9d3319e5a60668a2dc3d48de0bd1
Abhishek Kumar <abhishekcusat21@gmail.com>
2025-05-07 10:18:24 +0530

    addFirst

--- dataStruct/src/linkedlist/singlelinkedlist/SingleLinkedListCreation.java
+++ dataStruct/src/linkedlist/singlelinkedlist/SingleLinkedListCreation.java
@@ -1,4 +1,4 @@
-package linkedlist;
+package linkedlist.singlelinkedlist;
 
 public class SingleLinkedListCreation {
 
@@ -15,6 +15,24 @@ public class SingleLinkedListCreation {
     public static Node head;
     public static Node tail;
 
+    /*Add First in LinkedList
+      1-Create a new Node
+      2-new node's next = head
+      3-head = newNode
+    */
+    public void addFirst(int data){
+        //Step 1 Create new Node
+        Node newNode = new Node(data);
+        if( head == null){
+            head = tail = newNode;
+            return;
+        }
+        //Step 2 newNode next = head
+        newNode.next = head;
+        //Step 3 head = newNode
+        head = newNode;
+    }
+    
     public static void main(String[] args){
 
         SingleLinkedListCreation ll = new SingleLinkedListCreation();
